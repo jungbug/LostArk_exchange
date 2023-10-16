fd2a778fe8b213599255de9744f3353a2482a788
JUNG SUNG YOON <xbox1111@naver.com>
2023-10-17 02:41:04 +0900

    add: boxshadow

--- src/views/main/components/Main.tsx
+++ src/views/main/components/Main.tsx
@@ -13,14 +13,14 @@ export const Main = defineComponent({
             {MainData.map(({label, loginRequired, explain}, i, arr) => {
               return (
                 <li
-                class="mt-20 h-6 mx-2 shadow-slate-900"  
+                class="mt-20 h-6 mx-2 "   
                 style={{
                   width: 100 / arr.length + '%'
                 }}
                 onClick={() => {
                   moveTab(loginRequired, i+1); // 이거는 pinia에 있고 여기서는 호출만
                 }}>
-                    <div class="text-center w-full bg-white">
+                    <div class="text-center w-full bg-white shadow-2xl shadow-slate-300 rounded-xl p-2">
                       <div>
                         <img src="" alt="" />
                       </div>
